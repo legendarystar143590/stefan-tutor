e09d3139cb7ce191aa2b3448893596c17da5f711
Buzuev1234 <buzuevaleksandr71@gmail.com>
2023-10-03 09:41:22 +0900

    Initialize with CortexSteps

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e4dc15ca465e62d3"; }
+/******/ 		__webpack_require__.h = function() { return "7ec2fa14ccb982cd"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
